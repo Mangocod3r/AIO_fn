a8a59262fc47ccb5a30254eb3145607bc49ba5ff
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2024-01-23 16:42:59 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post(`${process.env.BACKEND}/login`, {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND}/login`, {
         name,
         password,
       });
