ebcaff9900c828bed8419af01c701efa55c4c808
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2024-01-23 16:40:31 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -17,7 +17,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${process.env.BACKEND}/register`, {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND}/register`, {
         name,
         password,
         email,
